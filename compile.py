1055cf384907cc54d86d154730045059c7aedded
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2024-08-17 16:45:04 +1000

    The colour compile thing does something perhaps maybe

--- compile.py
+++ compile.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -21,20 +21,50 @@ BLOCK_WIDTH = 400
 BLOCK_START = (PAPER_WIDTH - BLOCK_WIDTH) // 2 # x
 BLOCK_END = PAPER_WIDTH - BLOCK_START # x
 BLOCK_HEIGHT = 50
+BLOCKS_PER_PAGE = 8
+
+PAGE_TEXT_COLOUR = "#454c5a"
+CODE_TEXT_COLOUR = "#454c5a"
+
+A4_WIDTH = 595
+A4_HEIGHT = 842
+
 
 # TODO: this doesn't work properly if there's any invalid characters
 code = input()
-paper_height = (len(code) * 2 + 1) * BLOCK_HEIGHT
+paper_height = (((len(code) * 2 + 1) * BLOCK_HEIGHT) // A4_HEIGHT) + 2
 
-# Create a new image
-image = Image.new("RGB", (PAPER_WIDTH, paper_height), "white")
-draw = ImageDraw.Draw(image)
+page_font = ImageFont.load_default(size=40)
+code_font = ImageFont.load_default(size=60)
 
-for i, c in enumerate(code):
-    if c in MAPPINGS:
-        y_start = BLOCK_HEIGHT * (2 * i + 1)
-        draw.rectangle((BLOCK_START, y_start, BLOCK_END, y_start + BLOCK_HEIGHT), MAPPINGS[c])
+i = 0
+image_array = []
+
+for page in range(paper_height):
+    # Create a new image
+    image = Image.new("RGB", (A4_WIDTH, A4_HEIGHT), "white")
+    draw = ImageDraw.Draw(image)
+
+    draw.text((5, 0), str(page), font=page_font, fill=PAGE_TEXT_COLOUR)
     
+    for _ in range(BLOCKS_PER_PAGE):
+        if i >= len(code):
+            break
 
-# Show the image
-image.show()
+        c = code[i]
+        if c in MAPPINGS:
+            y_start = BLOCK_HEIGHT * (2 * (i % BLOCKS_PER_PAGE) + 1)
+            draw.rectangle((BLOCK_START, y_start, BLOCK_END, y_start + BLOCK_HEIGHT), MAPPINGS[c])
+
+            draw.text((BLOCK_END + 15, y_start - 15), c, font=code_font, fill=CODE_TEXT_COLOUR)
+
+        i += 1
+
+    image_array.append(image.copy())
+
+image_array[0].save("out.pdf", save_all=True, append_images=image_array[1:])
+print("PDF saved in cwd!")
+
+# for image in image_array:
+#     # Show the image
+#     image.show()
